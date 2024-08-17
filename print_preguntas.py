496f0fc50490d113fdb831547c2dd8bcb06df892
Carlos Silva Diaz <crasd15@live.com>
2024-08-17 16:14:45 -0400

    preguntas

--- print_preguntas.py
+++ print_preguntas.py
@@ -1,20 +1,17 @@
 import preguntas as p
 
 def print_pregunta(enunciado, alternativas):
-    
     # Imprimir enunciado y alternativas
-    ###############################################################
-    pass
-    
-    
-    
-    
-    ###############################################################
-        
+    print(enunciado[0])
+    opciones = ['A', 'B', 'C', 'D']
+    for i, alt in enumerate(alternativas):
+        print(f'{opciones[i]}. {alt[0]}')
+
 if __name__ == '__main__':
     # Las preguntas y alternativas deben mostrarse según lo indicado
     pregunta = p.pool_preguntas['basicas']['pregunta_1']
     print_pregunta(pregunta['enunciado'],pregunta['alternativas'])
+
     
     # Enunciado básico 1
 
