dbc1c572a77956ff464e0b6f8ad13a77a425ddbc
Carlos Silva Diaz <crasd15@live.com>
2024-08-17 16:22:54 -0400

    validator

--- validador.py
+++ validador.py
@@ -1,19 +1,15 @@
-
 def validate(opciones, eleccion):
     # Definir validación de eleccion
-    ##########################################################################
-    pass
+    while eleccion not in opciones:
+        print(f'Opción no válida. Ingrese una opción válida: {opciones}')
+        eleccion = input('Ingresa una opción: ').lower()
     
-    ##########################################################################
     return eleccion
 
-
 if __name__ == '__main__':
-    
     eleccion = input('Ingresa una Opción: ').lower()
-    # letras = ['a','b','c','d'] # pueden probar con letras también para verificar su funcionamiento.
-    numeros = ['0','1']
-    # Si se ingresan valores no validos a eleccion debe seguir preguntando
+    numeros = ['0', '1']
     validate(numeros, eleccion)
+
     
     
